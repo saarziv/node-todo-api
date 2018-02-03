9a2023b00256e9578b9b829156ab3853e79228b3
saar <saarzivon@gmail.com>
2018-02-03 02:34:22 +0200

    added remote db , deploying app to heroku

--- server/db/mongo-connect.js
+++ server/db/mongo-connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbName = "TodoApp";
-const url = `mongodb://localhost:27017/${dbName}`;
+const url = "mongodb://saarziv:Aa123456@ds123718.mlab.com:23718/node-todo-api-db" || `mongodb://localhost:27017/${dbName}`;
 
 mongoose.connect(url);
 
