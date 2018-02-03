242620c885956d9f05c1f90b773d07e71cdb8657
saar <saarzivon@gmail.com>
2018-02-03 03:25:49 +0200

    changed db conn again for it to work

--- server/db/mongo-connect.js
+++ server/db/mongo-connect.js
@@ -5,7 +5,7 @@ const dbUrl = {
     mlab:"mongodb://saarziv:Aa123456@ds123718.mlab.com:23718/node-todo-api-db",
     localhost:`mongodb://localhost:27017/${dbName}`
 };
-const url = dbUrl.localhost|| db.mlab;
+const url = dbUrl.localhost|| dbUrl.mlab;
 
 mongoose.connect(url);
 
