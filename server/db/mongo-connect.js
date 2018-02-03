e04b64977b9a4dde55c66846435b5f78ad1f3ba7
saar <saarzivon@gmail.com>
2018-02-03 03:11:25 +0200

    changing db uri (maybe wont work)

--- server/db/mongo-connect.js
+++ server/db/mongo-connect.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const dbName = "TodoApp";
-const url = "mongodb://saarziv:Aa123456@ds123718.mlab.com:23718/node-todo-api-db" || `mongodb://localhost:27017/${dbName}`;
+const remoteMongoDbUri = "mongodb://saarziv:Aa123456@ds123718.mlab.com:23718/node-todo-api-db";
+const url = process.env.MONGODB_URI || `mongodb://localhost:27017/${dbName}`;
 
 mongoose.connect(url);
 
