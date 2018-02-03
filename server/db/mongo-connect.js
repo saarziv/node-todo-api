90cdea6e8127feb2c58ce08b2c7dd2cb4b561bc5
saar <saarzivon@gmail.com>
2018-02-03 03:37:32 +0200

    done db connenctions

--- server/db/mongo-connect.js
+++ server/db/mongo-connect.js
@@ -5,7 +5,13 @@ const dbUrl = {
     mlab:"mongodb://saarziv:Aa123456@ds123718.mlab.com:23718/node-todo-api-db",
     localhost:`mongodb://localhost:27017/${dbName}`
 };
-const url = (process.env.PORT) ?  dbUrl.mlab : dbUrl.localhost;
+//first i created an environment variable on heroku with this cmd :heroku config:set MONGODB_URI=mongodb://saarziv:Aa123456@ds123718.mlab.com:23718/node-todo-api-db
+//now checking if the environment variable exists - if not use localhost.
+const url = process.env.MONGODB_URI || dbUrl.localhost;
+
+//another option is to use this :
+//checks if the app is on heroku , use the mlab db if not use local db.
+// const url = (process.env.PORT) ?  dbUrl.mlab : dbUrl.localhost;
 
 mongoose.connect(url);
 
